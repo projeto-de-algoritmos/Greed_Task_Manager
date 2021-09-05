9455ccd4c512cd94172893ed66dc9f9d55d4c680
joaovitor3 <joaovytor0@gmail.com>
2021-09-05 03:18:58 -0300

    Add edit task modal

--- schedule_calendar/forms.py
+++ schedule_calendar/forms.py
@@ -1,6 +1,7 @@
 from django import forms
-from schedule_calendar.models import Task
+from django.core.exceptions import ValidationError
 
+from schedule_calendar.models import Task
 
 class TaskCreateForm(forms.ModelForm):
     name = forms.CharField(max_length=100, required=True)
@@ -10,3 +11,42 @@ class TaskCreateForm(forms.ModelForm):
     class Meta:
         model = Task
         exclude = ('id',)
+
+    def clean(self):
+        start_date = self.cleaned_data['start_date']
+        end_date = self.cleaned_data['end_date']
+
+        if start_date <= end_date:
+            raise forms.ValidationError("Erro ao criar tarefa. A data de início deve ser menor que a data do fim de uma tarefa.")
+
+        return self.cleaned_data
+
+class TaskEditForm(forms.ModelForm):
+    task_id = forms.IntegerField()
+    name = forms.CharField(max_length=100, required=True)
+    start_date = forms.DateTimeField(input_formats=["%d.%m.%Y %H:%M"], required=True)
+    end_date = forms.DateTimeField(input_formats=["%d.%m.%Y %H:%M"], required=False)
+
+    def save(self, commit=True):
+        task = Task.objects.get(id=self.cleaned_data["task_id"])
+        task.name = self.cleaned_data["name"]
+        task.start_date = self.cleaned_data["start_date"]
+        task.end_date = self.cleaned_data["end_date"]
+        if commit:
+            task.save()
+
+        return task
+
+    def clean(self):
+        start_date = self.cleaned_data['start_date']
+        end_date = self.cleaned_data['end_date']
+
+        if start_date <= end_date:
+            raise forms.ValidationError("Erro ao criar tarefa. A data de início deve ser menor que a data do fim de uma tarefa.")
+
+        return self.cleaned_data
+
+
+    class Meta:
+        model = Task
+        exclude = ("id",)
--- schedule_calendar/views.py
+++ schedule_calendar/views.py
@@ -1,8 +1,9 @@
 import json
 
 from django.shortcuts import render
+from django.contrib import messages
 
-from schedule_calendar.forms import TaskCreateForm
+from schedule_calendar.forms import TaskCreateForm, TaskEditForm
 from schedule_calendar.models import Task
 
 
@@ -12,6 +13,7 @@ def get_tasks():
         tasks_list = []
         for task in tasks:
             task_dict = {
+                "id": task.id,
                 "title": task.name,
                 "start": task.start_date.strftime('%Y-%m-%dT%H:%M:%SZ'),
                 "end": task.end_date.strftime('%Y-%m-%dT%H:%M:%SZ')
@@ -25,18 +27,36 @@ def get_tasks():
 def home_view(request):
     context = {}
     task_create_form = TaskCreateForm()
+    task_edit_form = TaskEditForm(auto_id="edit_%s")
 
     if request.POST:
-        if request.POST["action"] == "create_event":
-            form = TaskCreateForm(request.POST)
-            if form.is_valid():
-                form.save()
+        if request.POST["action"] == "create_task":
+            create_form = TaskCreateForm(request.POST)
+            if create_form.is_valid():
+                create_form.save()
                 task_create_form = TaskCreateForm()
             else:
-                task_create_form = form
+                messages.add_message(
+                    request,
+                    messages.ERROR,
+                    create_form.errors.as_text().split('* ')[-1]
+                )
+                task_create_form = create_form
+
+        if request.POST['action'] == "edit_task":
+            edit_form = TaskEditForm(request.POST)
+            if edit_form.is_valid():
+                edit_form.save()
+                task_edit_form = TaskEditForm(auto_id="edit_%s")
+            else:
+
+                task_edit_form = edit_form
 
     context["task_create_form"] = task_create_form
+    context["task_edit_form"] = task_edit_form
+
     all_tasks = get_tasks()
     context["tasks"] = all_tasks
+
 
     return render(request, "home.html", context)
